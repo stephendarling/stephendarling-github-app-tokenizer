842d1e15c8e50aca09aba60569459a137278f8a7
Stephen Darling <stephen.darling@dailypay.com>
2022-09-24 13:52:09 -0400

    Add output value

--- generate_token.py
+++ generate_token.py
@@ -1,7 +1,17 @@
 import requests
 import os
 import boto3
+import argparse
 
-app_id = os.environ.get('APP_ID')
+parser = argparse.ArgumentParser(description="Generate GitHub token from App Key + ID")
+parser.add_argument("app_id", type=str, help="ID of the GitHub app")
+parser.add_argument("private_key", type=str, help="Private key associated with the GitHub app")
+args = parser.parse_args()
 
-print(f'Env var APP_ID = {app_id}')
+app_id = args.app_id
+private_key = args.private_key
+
+print(f'Arg for app_id = {app_id}')
+print(f'Arg for private_key = {private_key}')
+
+print(f"::set-output name=token::{app_id}+{private_key}")
