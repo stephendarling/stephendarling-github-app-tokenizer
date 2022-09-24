1a7f7fc38cb5bbb33f960642d18a4e6e067cb342
Stephen Darling <stephen.darling@dailypay.com>
2022-09-24 14:09:47 -0400

    test using sys.exit

--- generate_token.py
+++ generate_token.py
@@ -2,6 +2,7 @@ import requests
 import os
 import boto3
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description="Generate GitHub token from App Key + ID")
 parser.add_argument("app_id", type=str, help="ID of the GitHub app")
@@ -10,8 +11,5 @@ args = parser.parse_args()
 
 app_id = args.app_id
 private_key = args.private_key
-
-print(f'Arg for app_id = {app_id}')
-print(f'Arg for private_key = {private_key}')
-
-print(f"::set-output name=token::{app_id}{private_key}")
+token = f'{app_id}---{private_key}'
+sys.exit(token)
